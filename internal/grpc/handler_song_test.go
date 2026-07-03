d4679371b6650a02562ecf0974c9fba307de8862
inkOrCloud <inkorcloud@outlook.com>
2026-07-03 14:54:18 +0800

    fix: replace net.Listen with net.ListenConfig in song test for noctx

--- internal/grpc/handler_song_test.go
+++ internal/grpc/handler_song_test.go
@@ -7,15 +7,14 @@ import (
 
 	entsql "entgo.io/ent/dialect/sql"
 	"github.com/google/uuid"
-	_ "github.com/mattn/go-sqlite3"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-
+	songpb "github.com/inkOrCloud/EchoVault/echovault-server/api/grpc/generated/echo_vault/song/v1"
 	"github.com/inkOrCloud/EchoVault/echovault-server/internal/ent"
 	"github.com/inkOrCloud/EchoVault/echovault-server/internal/ent/enttest"
 	evgrpc "github.com/inkOrCloud/EchoVault/echovault-server/internal/grpc"
 	"github.com/inkOrCloud/EchoVault/echovault-server/internal/service/song"
-	songpb "github.com/inkOrCloud/EchoVault/echovault-server/api/grpc/generated/echo_vault/song/v1"
+	_ "github.com/mattn/go-sqlite3"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func newSongTestServer(t *testing.T) (songpb.SongServiceClient, func()) {
@@ -25,14 +24,16 @@ func newSongTestServer(t *testing.T) (songpb.SongServiceClient, func()) {
 		t.Fatalf("open db: %v", err)
 	}
 	client := enttest.NewClient(t, enttest.WithOptions(ent.Driver(drv)))
-	if err := client.Schema.Create(context.Background()); err != nil {
+	err = client.Schema.Create(context.Background())
+	if err != nil {
 		t.Fatalf("create schema: %v", err)
 	}
 	svc := song.NewService(client)
 	handler := evgrpc.NewSongHandler(svc)
 	s := grpc.NewServer()
 	songpb.RegisterSongServiceServer(s, handler)
-	lis, err := net.Listen("tcp", "127.0.0.1:0")
+	lc := net.ListenConfig{}
+	lis, err := lc.Listen(context.Background(), "tcp", "127.0.0.1:0")
 	if err != nil {
 		t.Fatalf("listen: %v", err)
 	}
